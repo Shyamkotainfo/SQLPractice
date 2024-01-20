cca19eac90c0e1d6a67d76f87b58b5a5f4e4f3a5
Shyamkotainfo <157146216+Shyamkotainfo@users.noreply.github.com>
2024-01-20 10:05:02 +0530

    Update Day3.sql

--- Day3.sql
+++ Day3.sql
@@ -28,3 +28,42 @@ FROM payment
 WHERE amount != 0
 GROUP BY staff_id
 ORDER BY SUM(amount) DESC;
+
+-- Challenge: GROUP BY multiple colums
+-- There are two competitions the two employess.
+-- Which employee had the highest sales amount in a single day?
+SELECT 
+staff_id,
+DATE (payment_date),
+SUM (amount),
+COUNT (*)
+FROM payment
+GROUP BY staff_id, DATE(payment_date)
+ORDER BY COUNT(*) DESC;
+
+-- Whihc employee had the most sales in a single day (not counting payments with amount = 0)?
+SELECT 
+staff_id,
+DATE (payment_date),
+SUM (amount),
+COUNT (*)
+FROM payment
+WHERE amount != 0
+GROUP BY staff_id, DATE(payment_date)
+ORDER BY COUNT(*) DESC;
+
+-- Challenge: HAVING
+-- In 2020, April 28, 29 and 30 were days with very high revenue. That's why we want
+-- to foucs in this task only on these days (filter accordingly).
+-- Find our what is the average payment amount grouped by customer and day - consider
+-- only the days/customers with more than 1 payment (per customer and day).
+-- Order by the average amount in a descending order.
+SELECT
+customer_id,
+DATE(payment_date),
+COUNT(payment_id),
+ROUND(AVG(amount), 2) AS AVG
+FROM payment
+WHERE DATE(payment_date) BETWEEN '2020-04-28' AND '2020-04-30 23:59'
+GROUP BY customer_id, DATE(payment_date) HAVING COUNT(payment_id) > 1
+ORDER BY AVG DESC;
