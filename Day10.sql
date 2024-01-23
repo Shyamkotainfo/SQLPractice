3f3a150ffa39b32c077b28f109255528388c8592
Shyamkotainfo <157146216+Shyamkotainfo@users.noreply.github.com>
2024-01-24 01:45:25 +0530

    Update Day10.sql

--- Day10.sql
+++ Day10.sql
@@ -50,3 +50,42 @@ ON b.category_id = c.category_id
 SELECT * FROM films_category
 WHERE name IN ('Action', 'Comedy');
 
+-- Challenge: Managing views
+-- In this challenge, we use again the view v_customer_info that we have previously created:
+CREATE VIEW v_customer_info
+AS
+SELECT cu.customer_id,
+    cu.first_name || ' ' || cu.last_name AS name,
+    a.address,
+    a.postal_code,
+    a.phone,
+    city.city,
+    country.country
+     FROM customer cu
+     JOIN address a ON cu.address_id = a.address_id
+     JOIN city ON a.city_id = city.city_id
+     JOIN country ON city.country_id = country.country_id
+ORDER BY customer_id
+-- You need to perform the following tasks:
+
+-- 1) Rename the view to v_customer_information.
+ALTER VIEW v_customer_info 
+RENAME TO v_customer_information
+-- 2) Rename the customer_id column to c_id.
+ALTER VIEW v_customer_information
+RENAME COLUMN customer_id TO c_id
+-- 3) Add also the initial column as the last column to the view by replacing the view.
+CREATE OR REPLACE VIEW v_customer_info
+AS
+SELECT cu.customer_id,
+    cu.first_name || ' ' || cu.last_name AS name,
+    a.address,
+    a.postal_code,
+    a.phone,
+    city.city,
+    country.country, cu.initials
+     FROM customer cu
+     JOIN address a ON cu.address_id = a.address_id
+     JOIN city ON a.city_id = city.city_id
+     JOIN country ON city.country_id = country.country_id
+ORDER BY customer_id
